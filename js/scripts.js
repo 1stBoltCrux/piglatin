334330c36d9a5722ab2a88649741d612fde9c4d6
Susan Southall and Ted Leary <ted@>
2018-03-28 11:46:01 -0700

    add functionality for checking 2 consonants

--- js/scripts.js
+++ js/scripts.js
@@ -5,14 +5,21 @@ $(document).ready(function(){
     var sentence = $("#input").val();
     // var vowels = ["a", "e", "i", "o", "u", "y"];
     // var sentence = "Hello World";
+    var newWord = "dog".slice(0,2);
 
       // debugger;
         // this checks if the first letter is a vowel
-        if(sentence[0] === "a" || sentence[0] === "e" || sentence[0]=== "i" || sentence[0]=== "o" || sentence[0] === "u" || sentence[0] === "y") {
-          alert("got it");
+        for(var i=0; i<newWord.length; i++){
+          vowelCheck(newWord[i]);
+        }
 
+        function vowelCheck(toCheck){
+          if(toCheck[0] === "a" || toCheck[0] === "e" || toCheck[0]=== "i" || toCheck[0]=== "o" || toCheck[0] === "u" || toCheck[0] === "y") {
+            alert("got it");
+        } else {
+          alert("consonant");
+        }
       }
-
       // // for (var i=0; i<newSentence.length; i++) {
       // // 	var word = newSentence[0];
       // //   for(var j =0; j<word.length; j++){
